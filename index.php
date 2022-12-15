c0c3f6f3ea308ad13e4c35af13631a419d294bce
Suporte Firesoft <suporte1.firesoft@gmail.com>
2022-12-15 18:01:07 -0300

    ADD: funções do primeiro digito

--- index.php
+++ index.php
@@ -1,22 +1,46 @@
 <?php
-//recebe o código do município, o numero de inscrição e o numero de ordem do estabelecimento
+//A = município, B = inscrição, C = ordem do estabelecimento
+//A1A2A3 B1B2B3B4B5B6 C1C2 D1D2
+$codigoInicial = "06230790400";
+$codigo1 = "";
 
-//declarando variaveis para receber os codigos em string
-$codigoInicial = "";
-$idMunicipio = "";
-$idInscricao = "";
-$ordemEstabelecimento = "";
+//acrescenta o 0 depois dos 3 primeiros numeros
+for ($i = 1; $i < 4; $i++) {
+    if ($i == 3) {
+        return $codigo1 = "062030790400";
+    }
+    return;
+}
 
-//recebe os numeros como string, acrescenta o 0 depois dos 3 primeiros numeros, converte pra numero e salva dentro de um array
+$arrStr = str_split($codigo1);
+$produto = 0;
+$soma = 0;
+$arrMod = [];
 
-//depois de receber num array, faz um foreach para multiplicar todos os itens em posição par por 1 e em posição impar por 2 e voltar eles pra um array
+for ($i = 0; $i < count($arrStr); $i++) {
+    $char = $arrStr[$i];
+    if ($i != 0 && ($i % 2) != 0) {
+        $produto = $char * 2;
+        array_push($arrMod, $produto);
+    } else {
+        array_push($arrMod, $char);
+    }
+}
 
-//alterar de um array para string e um foreach percorrendo e transformando cada um em numero e somando a alguma variavel soma
+$arrStrSoma = implode("", $arrMod);
 
-//verifica o valor soma para descobrir o valor da dezena exata mais próxima superior (talvez somar o primeiro digito +1 e substituir o segundo por 0)
+for ($i = 0; $i < strlen($arrStrSoma); $i++) {
+    $num = intval($arrStrSoma[$i]);
+    $soma += $num;
+}
 
-//acrescenta o resultado dessa subtraçao no final do valor inicial criando um array
+$dezenaSuperior = ceil($soma / 10) * 10;
+$digito1 = $dezenaSuperior - $soma;
+
+$codigoInicial += strval($digito1);
+//____________________________________
 
 //A segunda parte, pega o valor final da primeira parte, multiplicar da direita pra esquerda o array total -1 posição, e multiplica por dois, tira -1 e multiplica por 3 até chegar no indice 0
-
+for ($i = count($codigoInicial); $i > 0; $i--) {
+}
 //soma os produtos das multiplicações num foreach em que cada item é somado a uma variavel
